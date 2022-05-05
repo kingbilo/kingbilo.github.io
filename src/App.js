9361a046065a4d69db90a78d9d90706337418911
Jamie Bilodeau <bilodeaujamiea@yahoo.com>
2022-05-05 18:31:10 -0400

    adding sign in

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import { Link, Route, Routes, } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
+import LoginForm from './LoginForm'
 
 function App() {
-  return (
-    <div>
-        <nav>
-            <ul id="navigation">
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-            </ul>
-        </nav>
-        <Routes>
-            <Route path="/" element={<Home />}>
-            </Route>
-                <Route path="/about" element={<About />}>
-            </Route>
-            <Route path="/contact" element={<Contact />}></Route>
-        </Routes>
-    </div>
+
+    return (
+        <div>
+            <nav>
+                <ul id="navigation">
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+            {LoginForm()}
+            <Routes>
+                <Route path="/" element={<Home />}>
+                </Route>
+                    <Route path="/about" element={<About />}>
+                </Route>
+                <Route path="/contact" element={<Contact />}></Route>
+            </Routes>
+            
+        </div>
   );
 }
 
